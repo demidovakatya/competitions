87cac6bc63ca8968723e796c4db3c4f4de43daff
Katya Demidova <demidovakatya@gmail.com>
2016-07-19 21:53:55 +0300

    start the exploratory data analysis

---
+++ eda.R
@@ -0,0 +1,10 @@
+library(dplyr)
+library(ggplot2)
+
+options(scipen = 99999)
+Sys.setlocale(category = "LC_ALL", locale = "utf8")
+
+setwd("/Users/mac/Documents/talkingdata-mobile-user-demographics")
+source("R/read_all_the_files.R")
+
+# ------------- gender.age
