02eaa789daab09b1050255e723cf5e7ddb24439b
Katya Demidova <demidovakatya@gmail.com>
2017-01-15 00:30:21 +0300

    update

--- fake-news/tree.R
+++ fake-news/tree.R
@@ -1,8 +1,10 @@
 library(rpart)
 
-fake <- read.csv('data/fake.csv', stringsAsFactors = T)
+tree <- rpart(formula = type ~ published + published.weekday + language + country + spam_score +
+                  replies_count + participants_count + likes + comments + shares,
+              data = fake)
+rpart.plot::prp(tree)
 
-fake <- fake[-1]
-
-tree <- rpart(formula = type ~., data = fake, )
-plot(tree)
+tree.word.count <- rpart(formula = type ~ author_words + text_words + title_words,
+              data = fake)
+rpart.plot::prp(tree.word.count)
--- fake-news/untitled.R
+++ fake-news/untitled.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(lubridate)
+library(ggplot2)
 
 fake <- read.csv('data/fake.csv', stringsAsFactors = T)
 raw.fake <- fake
@@ -17,6 +18,28 @@ table(fake$type) # target
 table(fake$ord_in_thread)
 hist(fake$ord_in_thread)
 
+
+table(fake$published)
+table(fake$crawled)
+# TODO
+# preprocess
+fake <- mutate(fake, published = ymd_hms(published))
+qplot(x = published, data = fake, fill = type, bins = 20)
+
+fake <- mutate(fake, crawled = ymd_hms(crawled))
+qplot(x = crawled, data = fake, fill = type, bins = 20)
+
+# Add weekdays
+fake <- mutate(fake, published.weekday = weekdays(published))
+fake$published.weekday <- as.factor(fake$published.weekday)
+qplot(x = published.weekday, data = fake, fill = type)
+fake <- mutate(fake, crawled.weekday = weekdays(crawled))
+fake$crawled.weekday <- as.factor(fake$crawled.weekday)
+qplot(x = crawled.weekday, data = fake, fill = type)
+
+# fake <- mutate(fake, time.between.crawl.publ = hours(as.integer(crawled - published)))
+
+
 table(fake$author)
 # TODO:
 # Remove stuff like:
@@ -25,17 +48,18 @@ table(fake$author)
 fake <- mutate(fake, author.subd = tolower(sub(' \\| .*| \\(.*\\)', '', author)))
 fake$author.subd <- as.factor(fake$author.subd) # 59 levels less
 
-table(fake$published)
-table(fake$crawled)
-# TODO
-# preprocess
-fake <- mutate(fake, published = ymd_hms(published))
-fake <- mutate(fake, crawled = ymd_hms(crawled))
-# Add weekdays
-fake <- mutate(fake, published.weekday = weekdays(published))
-fake <- mutate(fake, crawled.weekday = weekdays(crawled))
-fake <- mutate(fake, published.weekday = weekdays(published))
-fake <- mutate(fake, crawled.weekday = weekdays(crawled))
-fake$crawled.weekday <- as.factor(fake$crawled.weekday)
-fake$published.weekday <- as.factor(fake$published.weekday)
+table(fake$title)
 
+# TODO
+# Extract text from html
+
+remove.html.tags <- function(s) {
+  return(gsub("<.+>", " ", s, ignore.case = T))
+}
+
+fake <- mutate(fake, title.text = remove.html.tags(title),
+                     text.text = remove.html.tags(text),
+                     author.text = remove.html.tags(author))
+
+# TODO
+# add word count for text, title, author
